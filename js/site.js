e5c1a91959d6d0ec97ea8b45f05a91dc75749348
Robert Went <i@robertwent.com>
2018-06-01 02:32:01 +0100

    Dropdown menu and touch behaviour

--- js/site.js
+++ js/site.js
@@ -1,3 +1,23 @@
-jQuery( document ).ready(function($) {
+jQuery(document).ready(function ($) {
+
+    //Menu hover behaviour for touchscreen devices
+    // Touch devices
+    if (Modernizr.touch) {
+        jQuery('.navbar-nav li.dropdown > a').click(function (e) {
+
+            //If a menu item is a parent and a link then prevent default if sub is hidden
+            if (!jQuery(this).closest('.navbar-nav li.dropdown').hasClass('show')) {
+                e.preventDefault();
+                jQuery(this).closest('.navbar-nav li.dropdown').addClass('show');
+            }
+        });
+
+        jQuery('body').click(function (e) {
+            //If there is a click elsewhere then remove the show class from parents
+            if (!jQuery(e.target).is('.navbar-nav li.dropdown > a')) {
+                jQuery('.navbar-nav li.dropdown').removeClass('show');
+            }
+        });
+    }
 
 });
