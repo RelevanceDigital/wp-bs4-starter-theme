e3772ee072e5c96cb490a019f557ccb67a601acb
Robert Went <i@robertwent.com>
2018-06-29 00:19:39 +0100

    Add clearfix to single pages incase of floated images

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'; ?>>
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
--- template-parts/content.php
+++ template-parts/content.php
@@ -9,7 +9,7 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<?php get_template_part( 'template-parts/schema/NewsArticle' ); ?>
 	<header class="entry-header">
 		<?php
