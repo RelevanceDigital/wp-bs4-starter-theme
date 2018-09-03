a0184ae5da4c4bd7f26c4d70d002c652fd2ec75d
Robert Went <i@robertwent.com>
2018-09-03 22:16:44 +0100

    Adds function for proper pagination
    For use in custom post archives

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -154,6 +154,26 @@ function _s_replace_image_lazy( $content ) {
 }
 
 /**
+ * Numbered pagination
+ */
+function _s_pagination_links() {
+    global $wp_query;
+
+    $total_pages = $wp_query->max_num_pages;
+
+    if ($total_pages > 1){
+        $current_page = max(1, get_query_var('paged'));
+
+        echo paginate_links(array(
+            'base' => get_pagenum_link(1) . '%_%',
+            'format' => '/page/%#%',
+            'current' => $current_page,
+            'total' => $total_pages,
+        ));
+    }
+}
+
+/**
  * Bootstrap comment form
  */
 function _s_comment_form( $args ) {
