1f73b84b1c8b7ffaaf8a7632195240d3a945049e
Robert Went <i@robertwent.com>
2019-03-23 02:00:38 +0000

    Accessibility fix

--- searchform.php
+++ searchform.php
@@ -1,5 +1,4 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-  <label for="search-box" class="sr-only"><?php echo _x( 'Search for:', 'label' ); ?></label>
-    <input type="search" id="search-box" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+    <input type="search" id="search-box" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder' ); ?>" value="<?php echo get_search_query(); ?>" name="s" aria-label="<?php echo _x( 'Search for:', 'label' ); ?>" />
   <input type="submit" class="search-submit btn btn-primary" value="<?php echo esc_attr_x( 'Search', 'submit button' ); ?>" />
 </form>
