c4f227b4874cdcd28c4d91fc000d7b677d430f9e
Robert Went <i@robertwent.com>
2019-08-25 20:31:34 +0100

    Don't remove core output by default
    Fixes #2

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -282,19 +282,19 @@ add_filter( 'emoji_svg_url', '__return_false' );
 
 //Disable the json api and remove the head link
 //add_filter('rest_enabled', '__return_false');
-add_filter( 'rest_jsonp_enabled', '__return_false' );
-remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+//add_filter( 'rest_jsonp_enabled', '__return_false' );
+//remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 
 //oEmbed stuff
 //Remove the REST API endpoint.
-remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+//remove_action( 'rest_api_init', 'wp_oembed_register_route' );
 //Turn off oEmbed auto discovery.
 //Don't filter oEmbed results.
-remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+//remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
 //Remove oEmbed discovery links.
-remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+//remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 //Remove oEmbed-specific JavaScript from the front-end and back-end.
-remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+//remove_action( 'wp_head', 'wp_oembed_add_host_js' );
 
 /**
  * Third party extensions that do annoying things
