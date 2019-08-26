bc5370fc85667bf53d6ce16a882793cc977f1467
Robert Went <i@robertwent.com>
2019-08-26 19:52:03 +0100

    Remove schema by default as it would break new Yoast implementation

--- template-parts/content.php
+++ template-parts/content.php
@@ -10,7 +10,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
-	<?php get_template_part( 'template-parts/schema/NewsArticle' ); ?>
+	<?php // get_template_part( 'template-parts/schema/NewsArticle' ); ?>
 	<header class="entry-header">
 		<?php
 		if ( is_singular() ) :
