e54541cbfee8d3bddfafbcb38526f1ebfc645682
Robert Went <i@robertwent.com>
2020-07-03 09:54:16 +0100

    focus-visible pollyfil

--- functions.php
+++ functions.php
@@ -242,6 +242,8 @@ function _s_scripts() {
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '0', true );
 
+	wp_enqueue_script( '_s-focus-visible-pollyfill', get_template_directory_uri() . '/assets/js/focus-visible.js', array(), '0', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
